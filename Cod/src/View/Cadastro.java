721fd9845223964b04af39eeb33b20f27616b797
Pedro Melo <141840576+PedroHMSilva1@users.noreply.github.com>
2024-05-14 20:42:39 -0300

    Add files via upload

---
+++ Cod/src/View/Cadastro.java
@@ -0,0 +1,98 @@
+import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Cadastro extends JFrame {
+    private JTextField nomeField, emailField, telefoneField, enderecoField;
+    private JPasswordField senhaField;
+    private JFormattedTextField dataNascimentoField;
+
+    public Cadastro() {
+        super("Cadastro");
+
+        JPanel panel = new JPanel(new GridLayout(7, 5));
+        panel.add(new JLabel("Nome:"));
+        nomeField = new JTextField(20);
+        panel.add(nomeField);
+
+        panel.add(new JLabel("Email:"));
+        emailField = new JTextField(20);
+        panel.add(emailField);
+
+        panel.add(new JLabel("Senha:"));
+        senhaField = new JPasswordField(10);
+        panel.add(senhaField);
+
+        panel.add(new JLabel("Telefone:"));
+        telefoneField = new JTextField(20);
+        panel.add(telefoneField);
+
+        panel.add(new JLabel("Data de Nascimento (dd/mm/aaaa):"));
+        MaskFormatter formatter = createFormatter("##/##/####");
+        dataNascimentoField = new JFormattedTextField(formatter);
+        panel.add(dataNascimentoField);
+
+        panel.add(new JLabel("Endereço:"));
+        enderecoField = new JTextField(20);
+        panel.add(enderecoField);
+
+        JButton cadastrarButton = new JButton("Cadastrar");
+        cadastrarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                confirmarCadastro();
+            }
+        });
+        panel.add(cadastrarButton);
+
+        add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private MaskFormatter createFormatter(String format) {
+        MaskFormatter formatter = null;
+        try {
+            formatter = new MaskFormatter(format);
+        } catch (java.text.ParseException exc) {
+            System.err.println("Formatter is bad: " + exc.getMessage());
+            System.exit(-1);
+        }
+        return formatter;
+    }
+
+    private void confirmarCadastro() {
+        int resposta = JOptionPane.showConfirmDialog(this, "Confirmar informações?", "Confirmação", JOptionPane.YES_NO_OPTION);
+        if (resposta == JOptionPane.YES_OPTION) {
+            cadastrar();
+        } else {
+            // Permitir ao usuário editar os dados
+        }
+    }
+
+    private void cadastrar() {
+        String nome = nomeField.getText();
+        String email = emailField.getText();
+        String senha = new String(senhaField.getPassword());
+        String telefone = telefoneField.getText();
+        String dataNascimento = dataNascimentoField.getText();
+        String endereco = enderecoField.getText();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Senha: " + senha);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Data de Nascimento: " + dataNascimento);
+        System.out.println("Endereço: " + endereco);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Cadastro();
+            }
+        });
+    }
+}
