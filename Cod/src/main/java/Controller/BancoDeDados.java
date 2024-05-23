b310a3c35c6ade969c3387028b5b9497f8222539
Guilherme H V Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2024-05-23 19:30:43 -0300

    Update BancoDeDados.java

--- Cod/src/main/java/Controller/BancoDeDados.java
+++ Cod/src/main/java/Controller/BancoDeDados.java
@@ -28,7 +28,7 @@ public class BancoDeDados {
 
     public static void main(String[] args) {
 
-        ImageIcon eventIcon = new ImageIcon("src\\Assets\\EventMaster.jpg");
+    ImageIcon eventIcon = new ImageIcon("src/main/java/Assets/EventMaster.jpg");
         try {
             String operacao[] = {"Cadastro de Usuário", "Login"};
             Object escolha = JOptionPane.showInputDialog(null, "O que deseja realizar?", "EventMaster", JOptionPane.QUESTION_MESSAGE, eventIcon, operacao, operacao[0]);
